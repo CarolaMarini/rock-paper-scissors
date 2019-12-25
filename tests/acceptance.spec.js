43c77b95cef300be4b84f61fb405ff3dc4295d4b
Carola <caro.marini@live.com.ar>
2019-12-25 20:17:19 -0300

    WIP - UI functionality for human player

--- tests/acceptance.spec.js
+++ tests/acceptance.spec.js
@@ -8,7 +8,14 @@ describe('Rock paper scissors', () => {
 
     describe('player vs computer', () => {
         it('Allows player to be human', async () => {
+            const humanSelector = "#human"
+            const humanOptsSelector = "#humanOpts"
             await expect(page.title()).resolves.toMatch('Rock Paper Scissors')
+            await page.waitForSelector(humanSelector);
+            await page.waitForSelector(humanOptsSelector, { visible: false })
+            await page.click(humanSelector);
+            await page.waitForSelector(humanOptsSelector, { visible: true })
+
         })
     })
 

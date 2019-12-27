cd495d1a1b5835050b088433e9c83f08d4e1afce
Carola <caro.marini@live.com.ar>
2019-12-27 14:29:32 -0300

    Refactors rock paper scissors route

--- routes.js
+++ routes.js
@@ -12,19 +12,19 @@ router.get('/computerPlay', (req, res) => {
 })
 
 router.post('/rps', (req, res) => {
+
     if (req.body.p1play === req.body.p2play) res.send("tie")
 
-    if (req.body.p1play === "scissors") {
-        if (req.body.p2play === "paper") res.send("Player 1 won!")
-        if (req.body.p2play === "rock") res.send("Player 2 won!")
-    }
-    if (req.body.p1play === "paper") {
-        if (req.body.p2play === "scissors") res.send("Player 2 won!")
-        if (req.body.p2play === "rock") res.send("Player 1 won!")
-    }
-    if (req.body.p1play === "rock") {
-        if (req.body.p2play === "scissors") res.send("Player 1 won!")
-        if (req.body.p2play === "paper") res.send("Player 2 won!")
+    switch (req.body.p1play) {
+        case ("scissors"):
+            if (req.body.p2play === "paper") res.send("Player 1 won!")
+            else res.send("Player 2 won!")
+        case ("paper"):
+            if (req.body.p2play === "scissors") res.send("Player 2 won!")
+            else res.send("Player 1 won!")
+        case ("rock"):
+            if (req.body.p2play === "scissors") res.send("Player 1 won!")
+            else res.send("Player 2 won!")
     }
 })
 
